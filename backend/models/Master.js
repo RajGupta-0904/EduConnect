de05e6e3066328c5246a75e3c7ac98776fa98f17
RajGupta-0904 <rajgupta1.0904@gmail.com>
2024-07-01 15:15:48 +0530

    added master scherma

---
+++ backend/models/Master.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+
+// Define the schema
+const MasterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ // Email validation regex
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6 
+    },
+    otp: {
+        type: String,
+        required: false
+    },
+    otpExpiration: {
+        type: Date,
+        required: false
+    },
+
+    mobile: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength:10,
+    
+        // match: /^\d{10}$/ // Mobile number validation regex (assuming 10 digit numbers)
+    },
+    
+});
+
+// Create the model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
